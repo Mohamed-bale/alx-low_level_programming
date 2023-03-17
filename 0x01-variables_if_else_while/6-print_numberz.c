156f743504f17d8b76e286198b46bed9651dbf0c
Mohamed-bale <mt7326748@gmail.com>
2023-03-17 14:05:06 -0700

    if

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+   *main - Entry point
+   *Discription: 'Check for number if it is positive or negative'
+   *Return: Always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
