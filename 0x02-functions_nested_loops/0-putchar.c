69b4670c6d323bb1b76b2476c443776827a90484
Mohamed-bale <mt7326748@gmail.com>
2023-03-21 13:03:14 -0700

    here we go

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
-   *main - Entry point
-   *Discription: 'Check for number if it is positive or negative'
-   *Return: Always 0
-  */
+ * main - Entry point
+ * Discription: 'print putchar'
+ * Return: Always 0
+ */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
