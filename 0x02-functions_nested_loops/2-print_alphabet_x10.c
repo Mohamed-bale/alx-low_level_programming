fd67bfb09485f83651f52a41e8cb130ab4d903b1
Mohamed-bale <mt7326748@gmail.com>
2023-03-21 13:23:09 -0700

    here we go

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Discription: 'Check for number if it is positive or negative'
+ * Return: Always 0
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; j <= 10; j++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n'):
+	}
+}
